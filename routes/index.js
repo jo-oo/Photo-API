279d8af9b5278ef23502b56d2558e016b1f3c987
Johanna <johannakoreo@gmail.com>
2022-03-09 10:20:57 +0100

    lagt till sökväg till user

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,13 @@ router.get('/', (req, res, next) => {
 	res.send({ success: true, data: { msg: 'oh, hi' }});
 });
 
-router.use('/example', require('./example'));
+router.get('/test', (req, res, next) => {
+
+	res.send({ success: true, data: { msg: 'test' }});
+});
+
+
+//här lägg sökvägar till
+router.use('/user', require('./user'));
 
 module.exports = router;
