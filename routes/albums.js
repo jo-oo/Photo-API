df5c6130b28cfc13a747c11f49df35962eae62f3
jo-oo <johannakoreo@gmail.com>
2022-04-12 22:06:42 +0200

    created all Album routes

---
+++ routes/albums.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { validationResult } = require('express-validator');
+const auth = require('../Middleware/authentication'); //läser in authentication där logik för Acess via JWT Token finns
+const router = express.Router();
+const albumController = require('../controllers/album_controller'); //Importerar user-controllern
+const albumValidationRules = require('../validation/album_validation_rules');//importerar validation-rules för user
+
+
+/* GET all albums */
+//1. GET from url http://localhost:3000/albums (albums-pathen sätter vi i slutet av index-filen)
+router.get('/', albumController.index);
+
+
+/* 2. GET Id*/
+//vid anrop till adressen http://localhost:3000/albums/:albumId så körs metoden show som ligger i album_controller-filen
+router.get('/:albumId', userController.showAlbum); 
+
+
+/* 3. STORE Spara ett nytt album */
+//vid anrop till adressen http://localhost:3000/albums/ så körs metoden STORE som ligger i album_controller-filen. Metoden lagrar det som skickats in via en POST-request
+router.post('/', userValidationRules.createUserRules, albumController.storeAlbum);
+
+
+/* 4. UPDATE  Uppdatera ett album */
+//vid anrop till adressen http://localhost:3000/albums/:albumId så körs metoden PUT som ligger i album_controller-filen. Metoden lagrar det som skickats in via en PUT-request
+router.put('/:albumId', albumValidationRules.updateRules, albumController.updateAlbum);
+
+/* 5. POST Lägg till ett foto i ett album */
+//vid anrop till adressen http://localhost:3000/albums/:albumId/photos så körs metoden POST som ligger i album_controller-filen. Metoden lagrar det som skickats in via en POST-request
+router.post('/:albumId/photos', albumValidationRules.addPhotoToAlbumRules, albumController.postPhoto);
+
+
+module.exports = router; //exporterar router-modulen
