d6345606ff4955f029a5ce302480dd823982fff2
jo-oo <johannakoreo@gmail.com>
2022-04-15 13:54:14 +0200

    initial requests works, testing status: success as repsonse

--- controllers/album_controller.js
+++ controllers/album_controller.js
@@ -1,8 +1,49 @@
 //Export methods
+
+const index = async (req, res) => {
+
+	res.send({
+		status: 'index',
+
+	});
+}
+
+const showAlbum = async (req, res) => {
+
+	res.send({
+		status: 'show album',
+
+	});
+}
+
+const storeAlbum = async (req, res) => {
+
+	res.send({
+		status: 'store album',
+
+	});
+}
+
+
+const updateAlbum = async (req, res) => {
+
+	res.send({
+		status: 'update album',
+
+	});
+}
+
+const postAlbum = async (req, res) => {
+
+	res.send({
+		status: 'post album',
+
+	});
+}
 module.exports = {
 	index,
 	showAlbum,
 	storeAlbum, //=register
 	updateAlbum,
-    postPhoto
+	postAlbum
 }
