71e9138a708f3f6001d6b7ae3deb8f8c2add4036
jo-oo <johannakoreo@gmail.com>
2022-04-17 14:51:43 +0200

    addedvrelations to photo

---
+++ models/photo_model.js
@@ -0,0 +1,19 @@
+module.exports = bookshelf => {
+	return bookshelf.model(
+		'Photo',
+		{
+			tableName: 'photos',
+			albums() {
+				return this.belongsToMany('Album');
+			},
+			users() {
+				return this.belongsTo('User');
+			},
+		},
+		{
+			async fetchById(id, fetchOptions = {}) {
+				return await new this({ id }).fetch(fetchOptions);
+			},
+		}
+	);
+};
